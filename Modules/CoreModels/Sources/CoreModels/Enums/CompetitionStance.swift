c91b5ec0f0a6b87148c5d67fb85e80ac2a61fe15
David Deng <tianpingdeng112233@gmail.com>
2026-07-12 22:50:04 +0100

    Fix resolver wiring lint (spec: 050-e1rm-single-source)

--- Modules/CoreModels/Sources/CoreModels/Enums/CompetitionStance.swift
+++ Modules/CoreModels/Sources/CoreModels/Enums/CompetitionStance.swift
@@ -4,6 +4,6 @@ import Foundation
 public enum CompetitionStance: String, Codable, Hashable, Sendable, CaseIterable {
   case lowBar = "low_bar"
   case highBar = "high_bar"
-  case conventional = "conventional"  // swiftlint:disable:this redundant_string_enum_value
+  case conventional = "conventional"
   case sumo = "sumo"  // swiftlint:disable:this redundant_string_enum_value
 }
