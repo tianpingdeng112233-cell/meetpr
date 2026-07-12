ca07af10f53f5e50514677f96cedc082adc14614
David Deng <tianpingdeng112233@gmail.com>
2026-07-12 22:52:52 +0100

    Remove redundant stance lint suppression (spec: 050-e1rm-single-source)

--- Modules/CoreModels/Sources/CoreModels/Enums/CompetitionStance.swift
+++ Modules/CoreModels/Sources/CoreModels/Enums/CompetitionStance.swift
@@ -5,5 +5,5 @@ public enum CompetitionStance: String, Codable, Hashable, Sendable, CaseIterable
   case lowBar = "low_bar"
   case highBar = "high_bar"
   case conventional = "conventional"
-  case sumo = "sumo"  // swiftlint:disable:this redundant_string_enum_value
+  case sumo = "sumo"
 }
